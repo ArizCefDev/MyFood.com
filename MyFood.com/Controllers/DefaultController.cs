60ad24c76a10ccdd89314cd0c2242f13cf82d927
Ariz Ceferli <106917027+ArizCefDev@users.noreply.github.com>
2024-09-16 19:43:11 +0400

    Add project files.

---
+++ MyFood.com/Controllers/DefaultController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyFood.com.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyFood.com/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyFood.com.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
